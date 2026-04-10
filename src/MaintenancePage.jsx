a26e91abca1b1b2f11b24e41622b413df322ca89
dioverdm <dioverrivero677@gmail.com>
2026-04-10 02:36:15 -0400

    Redes sociales

--- src/MaintenancePage.jsx
+++ src/MaintenancePage.jsx
@@ -128,6 +128,18 @@ export default function MaintenancePage() {
                   <small>¡Únete a nuestro servidor!</small>
                 </span>
               </a>
+              
+              
+              <a href="https://facebook.com/comunidadhabbup" className="mp-btn-f" target="_blank" rel="noreferrer">
+                <svg fill="#ffffff" width="18" height="18" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 2.03998C6.5 2.03998 2 6.52998 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.84998C10.44 7.33998 11.93 5.95998 14.22 5.95998C15.31 5.95998 16.45 6.14998 16.45 6.14998V8.61998H15.19C13.95 8.61998 13.56 9.38998 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96C15.9164 21.5878 18.0622 20.3855 19.6099 18.57C21.1576 16.7546 22.0054 14.4456 22 12.06C22 6.52998 17.5 2.03998 12 2.03998Z"/>
+                </svg>
+                <span>
+                  Facebook
+                  <small>¡Síguenos en Facebook!</small>
+                </span>
+              </a>
+              
               <a href="https://x.com/comunidadhabbup" className="mp-btn-x" target="_blank" rel="noreferrer">
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
                   <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.744l7.736-8.861L1.683 2.25H8.08l4.255 5.624 5.909-5.624zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
